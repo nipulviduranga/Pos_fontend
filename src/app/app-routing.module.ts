27b42c491b54ad7f9bfd6b97f8c6a298cafec908
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-02-21 09:25:30 +0530

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerPageComponent} from './components/customer-page/customer-page.component';
+import {NotFoundPageComponent} from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CustomerPageComponent},
+  {path: 'Customer', component: CustomerPageComponent},
+  {path: '**', component: NotFoundPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerPageComponent } from './components/customer-page/customer-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
