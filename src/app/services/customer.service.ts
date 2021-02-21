94e8d060880cd7cbd7f53302a35c4644ce18a4ef
seekerscloudlk <seekerscloudlkinfo@gmail.com>
2021-02-21 12:10:20 +0530

    6

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -11,7 +11,7 @@ export class CustomerService {
   constructor(private http: HttpClient) { }
 
   public saveCustomer(customer: CustomerDTO): Observable<any>{
-    return this.http.post('http://127.0.0.1:3000/api/v1/customerRoute/saveCustomer', {
+    return this.http.post('http://localhost:3000/api/v1/customerRoute/saveCustomer', {
       name: customer.name,
       address: customer.address,
       salary: customer.salary,
